a68ee199810c82a1ef7343aaa19c82327b5d2e22
leonvic13 <agencydreammaker@gmail.com>
2024-10-14 18:52:37 -0600

    Created inventory_app with basic view and URL routing (no models)

---
+++ erp/inventory_app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Welcome to the Inventory App!")
+
+
+# Create your views here.
