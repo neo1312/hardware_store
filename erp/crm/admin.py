0199d47af8eefc1822ce5cc8e2bf06be43b27254
Carlos <hdf@gmail.com>
2024-10-15 15:42:36 -0600

    agregue modelo venta

--- erp/crm/admin.py
+++ erp/crm/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import Client
+from .models import Client,Venta
 
 class ClientAdmin(admin.ModelAdmin):
     list_display = ('client_id', 'name', 'phone', 'monedero')  # Añadir monedero a la vista de lista
 
+class VentaAdmin(admin.ModelAdmin):
+    list_display = ('id','cliente','tipo','monto','fecha_creacion')  # Añadir monedero a la vista de lista
+
 admin.site.register(Client, ClientAdmin)
+admin.site.register(Venta, VentaAdmin)
 
--- erp/crm/models.py
+++ erp/crm/models.py
@@ -5,8 +5,18 @@ class Client(models.Model):
     name = models.CharField(max_length=100)
     phone = models.CharField(max_length=15)
     created_at = models.DateTimeField(auto_now_add=True)
-    monedero = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)  # Campo para el saldo
+    monedero = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
 
     def __str__(self):
         return self.name
+
+class Venta(models.Model):
+    id = models.AutoField(primary_key=True)
+    cliente = models.ForeignKey(Client, on_delete=models.CASCADE)  # Relación con el modelo Client
+    tipo = models.CharField(max_length=100)  # Tipo de venta (puedes definir las opciones que necesites)
+    monto = models.DecimalField(max_digits=10, decimal_places=2)  # Monto de la venta
+    fecha_creacion = models.DateTimeField(auto_now_add=True)  # Fecha de creación
+
+    def __str__(self):
+        return f"Venta {self.id} - {self.cliente.name} - {self.tipo}"
 
